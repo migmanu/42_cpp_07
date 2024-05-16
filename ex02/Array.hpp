51f7e7cf0423eaf28b91fc6aaecd9851542828d2
Jose Manuel Migoya Bussolotti <jmanuelmigoya@gmai.com>
2024-05-16 17:38:50 +0200

    feat: write half implementations of Array

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -6,21 +6,24 @@
 /*   By: jmigoya- <jmigoya-@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 17:03:26 by jmigoya-          #+#    #+#             */
-/*   Updated: 2024/05/16 17:11:44 by jmigoya-         ###   ########.fr       */
+/*   Updated: 2024/05/16 17:28:02 by jmigoya-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-template <typename T>
-class Array {
-	public:
+template <typename T> class Array
+{
+  public:
 	Array(void);
 	Array(unsigned int i);
 	Array(const Array &src);
 	~Array(void);
-	
 
 	Array &operator=(const Array &rhs);
 	Array &operator[](unsigned int idx);
 
 	unsigned int size(void);
+
+  private:
+	T *_arr;
+	unsigned int _size;
 };
