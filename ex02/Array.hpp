a8e6222b60e5810a20cfc8e512498c900fa6a716
Jose Manuel Migoya Bussolotti <jmanuelmigoya@gmai.com>
2024-05-16 17:13:43 +0200

    fix: partial work done

---
+++ ex02/Array.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Array.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmigoya- <jmigoya-@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/16 17:03:26 by jmigoya-          #+#    #+#             */
+/*   Updated: 2024/05/16 17:11:44 by jmigoya-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+template <typename T>
+class Array {
+	public:
+	Array(void);
+	Array(unsigned int i);
+	Array(const Array &src);
+	~Array(void);
+	
+
+	Array &operator=(const Array &rhs);
+	Array &operator[](unsigned int idx);
+
+	unsigned int size(void);
+};
