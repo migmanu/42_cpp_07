0e0e1fae83b8c1a5446840a350596646b01c9903
Jose Manuel Migoya Bussolotti <jmanuelmigoya@gmai.com>
2024-06-06 19:46:03 +0200

    feat(Array): Refactor Array class and update main
    The Array class has been refactored to improve its functionality. The
    constructor, destructor, and assignment operator have been updated.
    The subscript operator now throws an exception when the index is out
    of range. A print function has been added to print the array elements.
    The main function has been updated to test the new functionality of
    the Array class.

--- ex02/Array.hpp
+++ ex02/Array.hpp
@@ -6,9 +6,11 @@
 /*   By: jmigoya- <jmigoya-@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 17:03:26 by jmigoya-          #+#    #+#             */
-/*   Updated: 2024/05/16 17:28:02 by jmigoya-         ###   ########.fr       */
+/*   Updated: 2024/05/17 14:19:17 by jmigoya-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <iostream>
 
 template <typename T> class Array
 {
@@ -19,11 +21,72 @@ template <typename T> class Array
 	~Array(void);
 
 	Array &operator=(const Array &rhs);
-	Array &operator[](unsigned int idx);
+	T &operator[](unsigned int idx);
 
-	unsigned int size(void);
+	unsigned int size(void) const;
+	void print(void) const;
 
   private:
 	T *_arr;
 	unsigned int _size;
 };
+
+template <typename T> Array<T>::Array(void) : _size(0)
+{
+	_arr = new T[_size];
+	return;
+}
+
+template <typename T> Array<T>::Array(unsigned int i) : _size(i)
+{
+	_arr = new T[_size];
+	return;
+}
+
+template <typename T> Array<T>::Array(const Array &src)
+{
+	this = src;
+	return;
+}
+
+template <typename T> Array<T>::~Array(void)
+{
+	delete _arr;
+	_arr = NULL;
+	return;
+}
+
+template <typename T> Array<T> &Array<T>::operator=(const Array<T> &rhs)
+{
+	if (this != &rhs)
+	{
+		this->_size = rhs._size;
+		this->_arr = new T[_size];
+		for (unsigned int i = 0; i < rhs._size; i++)
+		{
+			this->_arr[i] = rhs._arr[i];
+		}
+	}
+}
+
+template <typename T> T &Array<T>::operator[](unsigned int idx)
+{
+	if (idx >= _size)
+		throw std::out_of_range("Index out of range");
+
+	return _arr[idx];
+}
+
+template <typename T> unsigned int Array<T>::size(void) const
+{
+	return this->_size;
+}
+
+template <typename T> void Array<T>::print(void) const
+{
+	for (unsigned int i = 0; i < this->_size; i++)
+	{
+		std::cout << _arr[i] << " ";
+	}
+	return;
+}
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,4 +1,27 @@
+#include "Array.hpp"
+#include <exception>
+#include <iostream>
+
 int main(void)
 {
+	std::cout << std::endl << "INTEGERS" << std::endl;
+	unsigned int i = 3;
+	Array<int> a(i);
+	a.print();
+	std::cout << "test out range: " << std::endl;
+	try
+	{
+		a[3] = 1;
+		a.print();
+	}
+	catch (std::exception &e)
+	{
+		std::cout << e.what();
+	}
+	std::cout << std::endl << "STRINGS" << std::endl;
+
+
+
+
 	return 0;
 }
